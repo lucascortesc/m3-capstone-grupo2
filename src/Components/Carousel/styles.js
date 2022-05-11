1fe98840e297027223b99b36761a2795641e052e
Anderson Vieira <andersonrvr@gmail.com>
2022-05-11 18:11:34 -0300

    feat: mixed home and carousel, still need header and footer

--- src/Components/Carousel/styles.js
+++ src/Components/Carousel/styles.js
@@ -1,16 +1,26 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background-color: var(--primaryColor50);
-  text-align: end;
-
+  background-color: var(--primaryColor);
   button {
-    margin: 10px 20px 0 20px;
     padding: 10px;
-    background-color: #193e51;
+    background-color: rgba(25, 62, 81, 0.6);
     color: #c3bd2e;
     border: none;
     border-radius: 5px;
+    display: none;
+  }
+  @media (min-width: 1024px) {
+    background-color: rgba(20, 102, 102, 0.4);
+    text-align: end;
+    display: flex;
+    flex-direction: column;
+    padding-top: 5px;
+    button {
+      display: block;
+      width: 200px;
+      margin: 0px 20px 0px 20px;
+    }
   }
 `;
 
@@ -18,10 +28,30 @@ export const ContainerCarousel = styled.div`
   display: flex;
   justify-content: space-between;
   overflow-x: scroll;
-  background-color: var(--primaryColor50);
+
   height: 250px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+
+  &::-webkit-scrollbar-track {
+    /* border: 1px solid #000; */
+    padding: 2px 0;
+    background-color: rgba(20, 102, 102, 0.4);
+  }
+
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    background-color: rgba(20, 102, 102, 0.7);
+    /* border: 1px solid #000; */
+  }
 
   @media (min-width: 1024px) {
     overflow-x: auto;
+    box-shadow: none;
+    overflow-y: hidden;
   }
 `;
