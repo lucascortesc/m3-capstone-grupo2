fd34a870dcf299e0fbea9645339390c95c759268
matheusprado1 <matheusprado1096@gmail.com>
2022-05-12 12:09:25 -0300

    feat: identation form

--- src/Pages/Register/index.jsx
+++ src/Pages/Register/index.jsx
@@ -18,15 +18,13 @@ export const Register = () => {
             <input></input>
             <div>
               <input type="checkbox" id="terms" name="terms"></input>
-              <label>
-                Concordo com os termos de política e privacidade.{" "}
-              </label>
+              <label>Concordo com os termos de política e privacidade. </label>
             </div>
+            <Button>Criar conta</Button>
+            <p>
+              Já tem uma conta?<Link to="/login">Clique aqui!</Link>
+            </p>
           </form>
-          <Button>Criar conta</Button>
-          <p>
-            Já tem uma conta?<Link to="/login">Clique aqui!</Link>
-          </p>
         </Styled.FormContainer>
       </Styled.Content>
     </Styled.Container>
