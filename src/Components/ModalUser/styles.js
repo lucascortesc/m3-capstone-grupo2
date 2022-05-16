d411ff2e370e18031a7dda0c3f75993ee5ee6945
matheusprado1 <matheusprado1096@gmail.com>
2022-05-16 18:56:37 -0300

    feat: update size mobile

--- src/Components/ModalUser/styles.js
+++ src/Components/ModalUser/styles.js
@@ -27,7 +27,7 @@ export const Container = styled.div`
 
   .modalContainer {
     width: 60%;
-    height: 40%;
+    height: 280px;
     border-radius: 12px;
     background-color: var(--primaryColor);
     display:flex;
