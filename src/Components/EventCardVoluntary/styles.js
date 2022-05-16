48f8a4608c2d550b83bb9c0f8bdfbc5a569177b0
lucascortesc <ccordeiro.lucas@gmail.com>
2022-05-16 20:37:12 -0300

    feat: Add remove event from userPage

--- src/Components/EventCardVoluntary/styles.js
+++ src/Components/EventCardVoluntary/styles.js
@@ -1,28 +1,28 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const Container = styled.div`
-min-width: 300px;
-max-width: 300px;
-border: 2px solid white;
-border-radius: 5px;
-font-family: "Montserrat";
-background-color: white;
-`
+  min-width: 300px;
+  max-width: 300px;
+  border: 2px solid white;
+  border-radius: 5px;
+  font-family: "Montserrat";
+  background-color: white;
+`;
 
 export const ImageContainer = styled.div`
-width: 100%;
-height: 200px;
-background-color: pink;
+  width: 100%;
+  height: 200px;
+  background-color: pink;
 
-img {
+  img {
     width: 100%;
     height: 100%;
-}
-`
+  }
+`;
 
 export const DescriptionContainer = styled.div`
-padding: 10px 0px 10px 20px;
-display: flex;
-flex-direction: column;
-gap: 10px;
-`
+  padding: 10px 0px 10px 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
