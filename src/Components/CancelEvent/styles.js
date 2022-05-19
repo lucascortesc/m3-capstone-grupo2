85a304aa2b242e8bee5ecd7c52b2ee97c0937fef
Anderson Vieira <andersonrvr@gmail.com>
2022-05-19 19:03:48 -0300

    fixed: modal cancel

--- src/Components/CancelEvent/styles.js
+++ src/Components/CancelEvent/styles.js
@@ -5,36 +5,39 @@ export const Container = styled.div`
   height: 100vh;
   position: fixed;
   top: 0;
+  left: 0;
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: rgb(0, 0, 0, 0.5);
   font-family: "Montserrat";
   color: white;
+  z-index: 1;
 
-  >div {
+  > div {
     width: 95%;
     display: flex;
     flex-direction: column;
     padding: 30px;
     gap: 10px;
     border-radius: 5px;
+    background-color: var(--primaryColor);
 
     .loading {
-        align-self: center;
-        margin-top: 10px 0px;
+      align-self: center;
+      margin-top: 10px 0px;
     }
 
     p {
-        font-weight: 700;
-        text-align: center;
-        text-shadow: 3px 3px 1px rgb(0, 0, 0, 0.8);
+      font-weight: 700;
+      text-align: center;
+      text-shadow: 3px 3px 1px rgb(0, 0, 0, 0.8);
     }
 
-    >div {
-        button {
-            text-shadow: 3px 3px 1px rgb(0, 0, 0, 0.8);
-        }
+    > div {
+      button {
+        text-shadow: 3px 3px 1px rgb(0, 0, 0, 0.8);
+      }
     }
 
     @media (min-width: 1024px) {
